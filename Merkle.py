0b734d1f6aa393f4c90c37c088379d8ca7d10f3a
AndreeaStancu <77406144+AndreeaStancu@users.noreply.github.com>
2025-05-20 18:57:46 +0300

    Update Merkle.py

--- Merkle.py
+++ Merkle.py
@@ -1,11 +1,27 @@
 from pymerkle import InmemoryTree as MerkleTree
+from pymerkle import verify_inclusion
 
 def compute_root_hash(leaves):
     tree = MerkleTree(algorithm='sha256')
     for leaf in leaves:
         tree.append_entry(leaf)
-    root_hash = tree.get_state()
-    return root_hash.hex()
+    root = tree.get_state(5)
+    return tree, root
+
+
+def verify_proof():
+    tree, root = compute_root_hash(leaves)
+    proof = tree.prove_inclusion(3, 5)
+    base = tree.get_leaf(3)
+    test=verify_inclusion(base, root, proof)
+    return test
+
 
 leaves = [b'mere', b'pere', b'portocale', b'afine', b'zmeura']
-print("Merkle Root:", compute_root_hash(leaves))
+tree, root = compute_root_hash(leaves)
+print("Merkle Root:", root.hex())
+try:
+    verify_proof()
+    print("Inclusion proof valid? True")
+except InvalidProof:
+    print("Inclusion proof valid? False")
